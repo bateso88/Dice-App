b71f44a7722fc61b0f4156d39af243ae77d2b095
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-07 12:03:45 +0000

    Working through second user story

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,9 +1,15 @@
 require 'dice'
 
 describe Dice do
+
   it 'Dice instances to repond to roll method' do
     dice = Dice.new
     expect(dice).to respond_to(:roll)
+  end
+
+  it 'roll should return a number between 1 and 6' do
+    dice = Dice.new
+    expect(dice.roll).to be_greater(1)
   end
 
 end
