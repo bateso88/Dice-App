7e46989d462a8584d0f706be3362ffca2f7fafa2
bateso88 <bateso@hotmail.co.uk>
2021-01-08 10:29:44 +0000

    refactored

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,12 +3,10 @@ require 'dice'
 describe Dice do
 
   it 'Dice instances to repond to roll method' do
-    dice = Dice.new
-    expect(dice).to respond_to(:roll)
+    expect(subject).to respond_to(:roll)
   end
 
   it 'roll should return a number between 1 and 6' do
-    #dice = Dice.new
     expect(subject.roll).to (be >=1).and (be <=6)
   end
 
